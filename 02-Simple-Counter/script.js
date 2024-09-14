13f3e29ba0b68591f1314557402369091c70175f
Saurabh Kumar <saurabhkumarxmpt@gmail.com>
2024-09-14 09:06:58 +0530

    complete the project

--- 02-Simple-Counter/script.js
+++ 02-Simple-Counter/script.js
@@ -1,10 +1,20 @@
-const Value=document.getElementById('Value');
 const buttons=document.getElementsByTagName('button');
-
-let value=0
-
-Value.innerHTML=value;
-
+let value=0;
 buttons[0].addEventListener('click',function(){
-    value==value-1
+    value=value-1;
+    if(value=="0"){
+        document.getElementById('Value').innerHTML="value is 0";
+    }
+    else{
+        document.getElementById('Value').innerHTML=value;
+    }
+})
+buttons[2].addEventListener('click',function(){
+    value=value+1;
+        document.getElementById('Value').innerHTML=value;
+
+})
+buttons[1].addEventListener('click',function(){
+    value=0;
+        document.getElementById('Value').innerHTML=value;
 })
